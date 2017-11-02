7ac76d955cb724e32b0b0ed78b4b6022ab21fea0
helenez <helene_199@hotmail.com>
2017-11-02 15:00:52 +0100

    Mood done

---
+++ app/src/main/java/gr04/cocox/MoodActivity.java
@@ -0,0 +1,13 @@
+package gr04.cocox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MoodActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mood);
+    }
+}
