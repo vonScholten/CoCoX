ff19e26c6310d5946cfc0f23063fd147a2c4eedb
Tardis <s154837@student.dtu.dk>
2018-01-03 17:13:31 +0100

    Lavede questions knapperne

--- app/src/main/java/gr04/cocox/QuestionsActivity.java
+++ app/src/main/java/gr04/cocox/QuestionsActivity.java
@@ -4,33 +4,96 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+
+import java.util.ArrayList;
 
 public class QuestionsActivity extends AppCompatActivity implements View.OnClickListener {
 
     ImageButton retur;
     ImageButton home;
 
+    Button selected;
+    Button refresh;
+
+    int activeIndex;
+
+    int[] btn_ID = {
+            R.id.doing,
+            R.id.time,
+            R.id.drink,
+            R.id.rounds,
+            R.id.what,
+            R.id.wrong_with_me,
+            R.id.why_here,
+            R.id.examination,
+            R.id.been_here,
+            R.id.plan,
+            R.id.where,
+            R.id.stuff
+    };
+
+    ArrayList<Button> inactive = new ArrayList<>(btn_ID.length);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questions);
+
         retur = (ImageButton) findViewById(R.id.retur);
         retur.setOnClickListener(this);
         home = (ImageButton) findViewById(R.id.home);
         home.setOnClickListener(this);
+
+        for (int id : btn_ID){
+            Button button = findViewById(id);
+            button.setOnClickListener(this);
+            inactive.add(button);
+        }
     }
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.retur:
-                finish();
-                break;
-            case R.id.home:
-                startActivity(new Intent(this, MainActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
-                break;
+        if(view == retur) {
+            finish();
         }
 
+        else if (view == home) {
+            startActivity(new Intent(this, MainActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+
+        }
+
+        else {
+            System.out.println("[1] DEBUG: button id: " + view.getId());
+            for (int id : btn_ID){
+
+                if(id == view.getId()){
+                    inactive.add(selected);
+                    selected = findViewById(id);
+                }
+            }
+            update();
+        }
+
+    }
+
+    public void update() {
+
+        activeIndex = inactive.indexOf(selected);
+
+        selected.setBackground(getDrawable(R.drawable.dialogue_active)); // set background to "activeIndex.xml"
+        selected.setTextColor(getColor(R.color.activeText)); // set text color
+        selected.setSelected(true); //Set button selected state as true
+        inactive.remove(activeIndex);
+
+        for (int id : btn_ID) {
+            if(inactive.contains(findViewById(id))) {
+                refresh = findViewById(id);
+                refresh.setBackground(getDrawable(R.drawable.dialogue_inactive_shadow));
+                refresh.setTextColor(getColor(R.color.inactiveText));
+                refresh.setSelected(false); //Set button selected state as false
+            }
+        }
     }
 }
