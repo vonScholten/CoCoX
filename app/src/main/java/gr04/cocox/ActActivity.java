c5d7cbe52b438e1743d8561228607b096df918c8
Tardis <s154837@student.dtu.dk>
2018-01-03 15:07:57 +0100

    Skygger på knapper i activities er lavet

--- app/src/main/java/gr04/cocox/ActActivity.java
+++ app/src/main/java/gr04/cocox/ActActivity.java
@@ -91,7 +91,7 @@ public class ActActivity extends AppCompatActivity implements View.OnClickListen
         for (int id : btn_ID) {
             if(inactive.contains(findViewById(id))) {
                 refresh = findViewById(id);
-                refresh.setBackground(getDrawable(R.drawable.dialogue_inactive));
+                refresh.setBackground(getDrawable(R.drawable.dialogue_inactive_shadow));
                 refresh.setTextColor(getColor(R.color.inactiveText));
                 refresh.setSelected(false); //Set button selected state as false
             }
