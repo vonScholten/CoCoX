d8853bdf13b8dba2d8a80e8052206ff34ffa5991
Tardis <s154837@student.dtu.dk>
2018-01-12 14:13:36 +0100

    Pop-up warning på reset knap

--- app/src/main/java/gr04/cocox/SettingsActivity.java
+++ app/src/main/java/gr04/cocox/SettingsActivity.java
@@ -1,10 +1,12 @@
 package gr04.cocox;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.Build;
 import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
@@ -49,12 +51,33 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
                 startActivity(new Intent(this, LanguageActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
                 break;
             case R.id.reset:
-                setupLanguage();
-                recreate();
-                break;
-        }
+                AlertDialog.Builder builder = new AlertDialog.Builder(SettingsActivity.this);
+                builder.setTitle("Reset");
+                builder.setMessage(R.string.resetbtn)
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                setupLanguage();
+                                recreate();
 
+                            }
+
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.dismiss();
+                            }
+
+                        });
+
+                builder.show();
+                break;
+
+
+        }
     }
+
     void setupLanguage() {
         Toast.makeText(this, "Resetter tablet", Toast.LENGTH_SHORT).show();
 
