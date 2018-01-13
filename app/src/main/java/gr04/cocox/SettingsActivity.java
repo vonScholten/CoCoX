315d18d49e905b4773ae05f19b18293f9195decb
vonScholten <f.vonscholten.reg@gmail.com>
2018-01-13 11:14:51 +0100

    select language moved to listview in settings activity

--- app/src/main/java/gr04/cocox/SettingsActivity.java
+++ app/src/main/java/gr04/cocox/SettingsActivity.java
@@ -11,21 +11,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Locale;
 
 public class SettingsActivity extends AppCompatActivity implements View.OnClickListener {
 
     ImageButton retur;
     ImageButton home;
+
     Button language;
     Button reset;
     Button sound;
 
     SharedPreferences sharedPreferences;
+
+    AlertDialog selectLang;
+
+    Locale myLocale;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,7 +66,8 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
                 startActivity(new Intent(this, MainActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
                 break;
             case R.id.language:
-                startActivity(new Intent(this, LanguageActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                //startActivity(new Intent(this, LanguageActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                selectLocal();
                 break;
             case R.id.reset:
                 AlertDialog.Builder builder = new AlertDialog.Builder(SettingsActivity.this);
@@ -66,7 +77,7 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
                             @Override
                             public void onClick(DialogInterface dialogInterface, int i) {
                                 resetPref();
-                                resetLanguage();
+                                resetLocal();
                                 recreate();
 
                             }
@@ -88,7 +99,77 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
         }
     }
 
-    public void resetLanguage() {
+    public void selectLocal() {
+        selectLang = new AlertDialog.Builder(this).create();
+        selectLang.setCancelable(true);
+        selectLang.setTitle(getString(R.string.select_lang));
+
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+
+        ListView listView = new ListView(this);
+
+        //The array containing strings of available languages (full name)
+        final String[] language = {
+                "Dansk",
+                "English"
+        };
+
+        //Use ISO 639-1 Code language code
+        final String[] languageCode = {
+                "da",
+                "en"
+        };
+
+        final ArrayList<String> listLocal = new ArrayList<String>();
+        for (String local : languageCode) { listLocal.add(local); }
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, language);
+        listView.setAdapter(adapter);
+
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+
+        layout.addView(listView);
+        selectLang.setView(layout);
+        selectLang.show();
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int pos, long id) {
+                setLocal(listLocal.get(pos));
+                selectLang.dismiss();
+                recreate();
+            }
+        });
+    }
+
+    public String loadLocale() {
+        String lang = sharedPreferences.getString("lang", "da");
+        System.out.println("lang: " + lang + " loaded");
+        return lang;
+    }
+
+    public void setLocal(String lang) {
+        if (lang.equalsIgnoreCase(""))
+            return;
+
+        myLocale = new Locale(lang);
+
+        saveLocale(lang);
+
+        Locale.setDefault(myLocale);
+        android.content.res.Configuration config = new android.content.res.Configuration();
+        config.locale = myLocale;
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+    }
+
+    public void saveLocale(String lang) {
+        sharedPreferences.edit().putString("lang",lang).apply();
+        System.out.println("lang: " + lang + " saved");
+    }
+
+    public void resetLocal() {
         Toast.makeText(this, "Resetter tablet", Toast.LENGTH_SHORT).show();
 
         Locale locale = new Locale("da", "DK");
@@ -103,4 +184,11 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
         sharedPreferences.edit().putString("lang","da").apply();
         sharedPreferences.edit().putInt("currentSound",1).apply();
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        setLocal(loadLocale());
+    }
+
 }
