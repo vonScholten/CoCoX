90e7b9e47c92d57514c875e1cc39e1e90c81da66
Tardis <s154837@student.dtu.dk>
2018-01-16 13:41:50 +0100

    Kald, retur og home virker i puzzle

--- app/src/main/java/gr04/cocox/PuzzleActivity.java
+++ app/src/main/java/gr04/cocox/PuzzleActivity.java
@@ -1,10 +1,12 @@
 package gr04.cocox;
 
+import android.content.Intent;
 import android.content.res.TypedArray;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import java.util.ArrayList;
@@ -14,6 +16,9 @@ public class PuzzleActivity extends AppCompatActivity implements View.OnClickLis
     Button hint;
     int index;
     boolean check;
+    ImageButton retur;
+    ImageButton home;
+    Button call;
 
     //TypedArray imgs = getResources().obtainTypedArray(R.array.random_imgs);
 
@@ -63,6 +68,12 @@ public class PuzzleActivity extends AppCompatActivity implements View.OnClickLis
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puzzle);
+        retur = findViewById(R.id.retur);
+        retur.setOnClickListener(this);
+        home = findViewById(R.id.home);
+        home.setOnClickListener(this);
+        call = findViewById(R.id.call);
+        call.setOnClickListener(this);
 
         for (int id : image_ID){
             ImageView iv = findViewById(id);
@@ -81,6 +92,20 @@ public class PuzzleActivity extends AppCompatActivity implements View.OnClickLis
 
     @Override
     public void onClick(View view) {
+
+        if(view == retur) {
+            finish();
+        }
+
+        else if (view == home) {
+            startActivity(new Intent(this, MainActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+
+        }
+
+        else if(view==call){
+            startActivity(new Intent(this, CallActivity.class));
+
+        }
 
         switch (view.getId()){
             case R.id.puzzle1 : checkPiecesArray(view);
